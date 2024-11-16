366fd5993ff3452645d719acec514bdd466ec70a
majdalaryann <majd.alaryann@gmail.com>
2024-11-16 19:41:01 +0300

    Task1-completed

--- cypress/e2e/task-1.cy.js
+++ cypress/e2e/task-1.cy.js
@@ -2,64 +2,60 @@
 
 describe("Task 1", ()=>{
     it("Conduit Logo Header", ()=>{
-        cy.visit("https://demo.productionready.io/#/register")
-        cy.get('.navbar-brand').contains('conduit')
+        cy.visit("/register")
+        //cy.get('.navbar-brand').contains('conduit')
+        cy.get("[href='#/']").first().contains('conduit')
         
     });
 
    it("Home Nav Link", ()=>{
-    cy.visit("https://demo.productionready.io/#/register")
+    cy.visit("/register")
     cy.get(".nav").children().eq(0).contains("Home")
    });
 
    it("Sign up Nav Link", ()=>{
-    cy.visit("https://demo.productionready.io/#/register")
+    cy.visit("/register")
     cy.get(".nav").children().eq(2).contains("Sign up")
     //cy.get("[href='#/register']").first()
    })
 
    it("Username field", ()=>{
-    cy.visit("https://demo.productionready.io/#/register")
+    cy.visit("/register")
     cy.get("[placeholder=Username]")
    });
 
    it("Email field", ()=>{
-    cy.visit("https://demo.productionready.io/#/register")
+    cy.visit("/register")
     cy.get("[type=email]")
    });
 
    it("password field", ()=>{
-    cy.visit("https://demo.productionready.io/#/register")
+    cy.visit("/register")
     cy.get("[placeholder=Password]")
    });
 
    it("Sign up button", ()=>{
-    cy.visit("https://demo.productionready.io/#/register")
+    cy.visit("/register")
     cy.get("[type=submit]")
    });
 
    it("Sign up text", ()=>{
-    cy.visit("https://demo.productionready.io/#/register")
-    cy.get(".text-xs-center").first()
+    cy.visit("/register")
+    cy.get(".text-xs-center").first().contains('Sign up')
    });
 
    it("Have an account? link", ()=>{
-    cy.visit("https://demo.productionready.io/#/register")
-    cy.get("p.text-xs-center")
+    cy.visit("/register")
+    cy.get("[href='#/login']").last()
    });
 
    it("Conduit Footer", ()=>{
-    cy.visit("https://demo.productionready.io/#/register")
+    cy.visit("/register")
     cy.get(".logo-font")
    });
 
    it("text in footer", ()=>{
-    cy.visit("https://demo.productionready.io/#/register")
+    cy.visit("/register")
     cy.get('.container .attribution')
    });
-
-
-
-
-
-})
+});
